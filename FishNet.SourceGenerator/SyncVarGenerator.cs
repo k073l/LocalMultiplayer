0c270efa7655e8625a88a8b56ebf883fce341618
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-04-11 01:55:31 +0200

    Added fishnet source generator project and added reference in RealRadio project

---
+++ FishNet.SourceGenerator/SyncVarGenerator.cs
@@ -0,0 +1,15 @@
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public class SyncVarGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+
+    }
+}
