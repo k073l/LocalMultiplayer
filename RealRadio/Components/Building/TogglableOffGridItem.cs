ddeac67660d4675dfca56beee884f46b41ee115d
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-04-15 22:57:39 +0200

    Removed testing code from TogglableOffGridItem

--- RealRadio/Components/Building/TogglableOffGridItem.cs
+++ RealRadio/Components/Building/TogglableOffGridItem.cs
@@ -9,30 +9,18 @@ namespace RealRadio.Components.Building;
 
 public class TogglableOffGridItem : OffGridItem
 {
-    //[field: SyncVar(Channel = FishNet.Transporting.Channel.Reliable, ReadPermissions = ReadPermission.Observers, WritePermissions = WritePermission.ServerOnly, OnChange = nameof(OnStateToggled))]
-    //public bool IsOn { get; [ServerRpc(RequireOwnership = false, RunLocally = true)] set; }
-
     [field: SyncVar(Channel = FishNet.Transporting.Channel.Reliable, ReadPermissions = ReadPermission.ExcludeOwner, WritePermissions = WritePermission.ServerOnly, OnChange = nameof(OnStateToggled))]
     public bool IsOn { get; [ServerRpc(RequireOwnership = false, RunLocally = true)] set; }
 
-    private float nextToggleTime = 0f;
-
-    private void OnStateToggled(bool prev, bool next, bool asServer)
+    protected virtual void OnStateToggled(bool prev, bool next, bool asServer)
     {
-        Plugin.Logger.LogInfo($"TogglableOffGridItem.IsOn changed from {prev} to {next} (asServer: {asServer})");
     }
 
-    public override void OnSpawnServer(NetworkConnection connection)
+    public override void OnStartClient()
     {
-        base.OnSpawnServer(connection);
+        base.OnStartClient();
 
-        Plugin.Logger.LogInfo("TogglableOffGridItem.OnSpawnServer");
-    }
-
-    [ObserversRpc]
-    private void BroadcastToClients(string message)
-    {
-        Plugin.Logger.LogInfo($"Incoming message: {message}");
+        Plugin.Logger.LogInfo($"IsOn: {IsOn}");
     }
 
     private void Update()
@@ -40,17 +28,6 @@ public class TogglableOffGridItem : OffGridItem
         if (isGhost)
             return;
 
-        if (NetworkObject.IsServer)
-        {
-            if (Time.time >= nextToggleTime)
-            {
-                nextToggleTime = Time.time + 1f;
 
-                var newValue = !IsOn;
-                Plugin.Logger.LogInfo($"Toggling IsOn to {newValue}");
-                IsOn = newValue;
-                BroadcastToClients($"Toggled IsOn to {IsOn}");
-            }
-        }
     }
 }
