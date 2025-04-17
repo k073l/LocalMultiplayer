19afd9b6b66b2c28775e0d823880f15ecc7cba46
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-04-18 01:02:02 +0200

    Fixed audio client events not always firing

--- RealRadio/Components/Audio/StreamAudioHost.cs
+++ RealRadio/Components/Audio/StreamAudioHost.cs
@@ -79,6 +79,7 @@ public class StreamAudioHost : MonoBehaviour
         if (client.isActiveAndEnabled)
         {
             enabledClients.Add(client);
+            OnNumActiveClientsChanged();
         }
 
         return client;
