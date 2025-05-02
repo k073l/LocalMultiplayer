a11989c2a8df17bec6c4846723b70f40c8566e48
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-02 04:23:56 +0200

    Show radial menu with radio stations when holding reload while in a vehicle (nothing happens yet)

--- RealRadio/Components/Radio/VehicleRadioManager.cs
+++ RealRadio/Components/Radio/VehicleRadioManager.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Collections.Generic;
+using RealRadio.Components.Building;
+using RealRadio.Components.UI;
 using RealRadio.Components.Vehicles;
 using RealRadio.Patches;
+using ScheduleOne;
 using ScheduleOne.DevUtilities;
+using ScheduleOne.PlayerScripts;
 using ScheduleOne.Vehicles;
 
 namespace RealRadio.Components.Radio;
 
 public class VehicleRadioManager : NetworkSingleton<VehicleRadioManager>
 {
+    private bool radialMenuOpen;
+
+    private InteractableOption[]? radialMenuOptions;
+
     public override void Awake()
     {
         base.Awake();
@@ -21,11 +30,70 @@ public class VehicleRadioManager : NetworkSingleton<VehicleRadioManager>
         LandVehicleStartPatch.OnVehicleSpawned += OnVehicleSpawned;
     }
 
+    public override void Start()
+    {
+        radialMenuOptions = CreateRadialMenuOptions();
+    }
+
+    private InteractableOption[] CreateRadialMenuOptions()
+    {
+        var options = new InteractableOption[RadioStationManager.Instance.Stations.Count];
+
+        for (int i = 0; i < RadioStationManager.Instance.Stations.Count; ++i)
+        {
+            var station = RadioStationManager.Instance.Stations[i];
+            options[i] = InteractableOption.CreateOption(
+                id: station.Url!,
+                name: station.Name!,
+                sprite: null
+            );
+        }
+
+        return options;
+    }
+
     private void OnVehicleSpawned(LandVehicle vehicle)
     {
+        if (!IsServer)
+            return;
+
         var proxyPrefab = Plugin.Assets!.Prefabs.VehicleRadioProxy;
         var proxy = Instantiate(proxyPrefab, parent: transform);
         proxy.GetComponent<VehicleRadioProxy>().Vehicle = vehicle;
         Spawn(proxy);
+    }
+
+    private void Update()
+    {
+        UpdateRadialMenu();
+    }
+
+    private void UpdateRadialMenu()
+    {
+        if (GameInput.GetButtonUp(GameInput.ButtonCode.Reload) || Player.Local.CurrentVehicle == null)
+        {
+            if (radialMenuOpen)
+            {
+                radialMenuOpen = false;
+                RadialMenu.Instance.Hide();
+            }
+
+            return;
+        }
+
+        if (!GameInput.GetButton(GameInput.ButtonCode.Reload))
+            return;
+
+        if (radialMenuOpen)
+            return;
+
+        if (radialMenuOptions == null)
+        {
+            Plugin.Logger.LogWarning("Radio station options are null");
+            return;
+        }
+
+        radialMenuOpen = true;
+        RadialMenu.Instance.Show(radialMenuOptions);
     }
 }
