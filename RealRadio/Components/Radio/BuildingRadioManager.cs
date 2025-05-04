b411f0a659d71c6c7f252fdbe6076a78e96100ff
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-04 16:47:07 +0200

    Only add audio clients to residential buildings and create a dictionary of npc residents in buildings on init

--- RealRadio/Components/Radio/BuildingRadioManager.cs
+++ RealRadio/Components/Radio/BuildingRadioManager.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using RealRadio.Components.Buildings;
 using ScheduleOne.DevUtilities;
 using ScheduleOne.Map;
+using ScheduleOne.NPCs;
+using ScheduleOne.NPCs.Schedules;
 using UnityEngine;
 
 namespace RealRadio.Components.Radio;
@@ -14,6 +16,37 @@ public class BuildingRadioManager : NetworkSingleton<BuildingRadioManager>
     public GameObject RadioProxyPrefab { get; private set; } = null!;
 
     public Dictionary<int, NPCEnterableBuilding> Buildings { get; private set; } = null!;
+    public Dictionary<NPCEnterableBuilding, HashSet<NPC>> Residents { get; } = [];
+
+    private static readonly string[] blackListedBuildingWords =
+    [
+        "pizzeria",
+        "office",
+        "pharmacy",
+        "store",
+        "warehouse",
+        "chemical",
+        "industrial",
+        "center",
+        "cafe",
+        "diner",
+        "bar",
+        "bank",
+        "chapel",
+        "tent",
+        "hut",
+        "arcade",
+        "restaurant",
+        "shooting",
+        "barber",
+        "nightclub",
+        "station",
+        "market",
+        "container",
+        "caravan",
+        "courthouse",
+        "townhall",
+    ];
 
     public override void Awake()
     {
@@ -21,26 +54,107 @@ public class BuildingRadioManager : NetworkSingleton<BuildingRadioManager>
 
         if (RadioProxyPrefab == null)
             throw new InvalidOperationException("RadioProxyPrefab is null");
+    }
 
+    public static int GetBuildingHash(NPCEnterableBuilding building)
+    {
+        int hash;
+
+        unchecked
+        {
+            hash = building.GUID.GetHashCode() * 31 * building.BuildingName.GetHashCode();
+        }
+
+        Plugin.Logger.LogInfo($"Building hash: {building.GUID} - {building.BuildingName} ({hash})");
+
+        return hash;
+    }
+
+    private void InitResidents()
+    {
+        var npcs = FindObjectsOfType<NPC>(includeInactive: true);
+        var buildings = new List<NPCEnterableBuilding>(capacity: 1);
+
+        foreach (var npc in npcs)
+        {
+            buildings.Clear();
+            var schedule = npc.GetComponentInChildren<NPCScheduleManager>();
+
+            if (schedule == null)
+                continue;
+
+            foreach (var action in schedule.ActionList)
+            {
+                if (action is not NPCEvent_StayInBuilding stayInBuilding)
+                    continue;
+
+                var building = stayInBuilding.Building;
+
+                // Skip non residential buildings
+                if (!BuildingIsResidential(building))
+                    continue;
+
+                buildings.Add(building);
+            }
+
+            foreach (var building in buildings)
+            {
+                if (!Residents.TryGetValue(building, out var residents))
+                {
+                    residents = new();
+                    Residents.Add(building, residents);
+                }
+
+                residents.Add(npc);
+            }
+        }
+    }
+
+    private void InitBuildings()
+    {
         var buildings = FindObjectsOfType<NPCEnterableBuilding>();
         Buildings = new(capacity: buildings.Length);
 
         foreach (var building in buildings)
         {
+            // Skip non residential buildings
+            if (!BuildingIsResidential(building))
+                continue;
+
             // Mick's house seems to have double components added for some reason, so we need to check if the building already exists
             if (!Buildings.TryAdd(GetBuildingHash(building), building))
             {
-                Plugin.Logger.LogWarning($"Building already exists: {building.GUID} - {building.BuildingName}");
+                Plugin.Logger.LogWarning($"Found duplicate building: {building.GUID} - {building.BuildingName}");
             }
         }
     }
 
-    public static int GetBuildingHash(NPCEnterableBuilding building)
+    private bool BuildingIsResidential(NPCEnterableBuilding building)
     {
-        unchecked
+        if (building is MedicalCentre)
+            return false;
+
+        if (building is PoliceStation)
+            return false;
+
+        var name = building.name.ToLowerInvariant();
+        var buildingName = building.BuildingName.ToLowerInvariant();
+
+        foreach (var word in blackListedBuildingWords)
         {
-            return building.GUID.GetHashCode() * 31 * building.BuildingName.GetHashCode();
+            if (name.Contains(word) || buildingName.Contains(word))
+                return false;
         }
+
+        return true;
+    }
+
+    public void AddProxy(BuildingRadioProxy proxy)
+    {
+        if (proxy.Building == null)
+            throw new InvalidOperationException("Proxy building is null");
+
+        Proxies.Add(proxy.Building, proxy);
     }
 
     public override void Start()
@@ -48,9 +162,23 @@ public class BuildingRadioManager : NetworkSingleton<BuildingRadioManager>
         base.Start();
     }
 
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        if (IsClientOnly)
+        {
+            InitBuildings();
+            InitResidents();
+        }
+    }
+
     public override void OnStartServer()
     {
         base.OnStartServer();
+
+        InitBuildings();
+        InitResidents();
 
         foreach (var building in Buildings.Values)
         {
