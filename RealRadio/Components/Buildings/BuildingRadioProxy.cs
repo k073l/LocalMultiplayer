5420abb7d76aecc74812c18cfe8152fbba1eadf8
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-04 04:35:44 +0200

    Send building info to clients and init audio client when building is received

--- RealRadio/Components/Buildings/BuildingRadioProxy.cs
+++ RealRadio/Components/Buildings/BuildingRadioProxy.cs
@@ -1,16 +1,70 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using FishNet.Connection;
 using FishNet.Object;
 using RealRadio.Components.Radio;
 using ScheduleOne.Map;
+using UnityEngine;
 
 namespace RealRadio.Components.Buildings;
 
 public class BuildingRadioProxy : RadioProxy
 {
-    private NPCEnterableBuilding? building;
+    public NPCEnterableBuilding? Building { get; set; }
 
-    private void Start()
+    protected override void InitAudioClient(bool delayStart = true)
     {
-        building = GetComponent<NPCEnterableBuilding>() ?? throw new InvalidOperationException("No NPCEnterableBuilding component found on game object");
+        if (IsClient && Building == null)
+        {
+            StartCoroutine(WaitForReceiveBuildingThenInitAudioClient());
+            return;
+        }
+
+        base.InitAudioClient(delayStart);
+
+        audioClient!.ConvertToMono = true;
+    }
+
+    private IEnumerator WaitForReceiveBuildingThenInitAudioClient()
+    {
+        yield return new WaitUntil(() => Building != null);
+        InitAudioClient();
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        RequestBuildingInfo();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void RequestBuildingInfo(NetworkConnection conn = null!)
+    {
+        if (Building == null)
+            throw new InvalidOperationException("Building is null");
+
+        ReceiveBuildingInfo(conn, BuildingRadioManager.GetBuildingHash(Building));
+    }
+
+    [TargetRpc]
+    private void ReceiveBuildingInfo(NetworkConnection conn, int buildingHash)
+    {
+        if (!BuildingRadioManager.Instance.Buildings.TryGetValue(buildingHash, out var building))
+            throw new InvalidOperationException($"Building not found: {buildingHash}");
+
+        Building = building;
+        OnBuildingSet();
+    }
+
+    private void OnBuildingSet()
+    {
+        if (Building == null)
+            throw new InvalidOperationException("Building is null");
+
+        audioClientObject = Instantiate(AudioClientPrefab);
+        audioClientObject.transform.SetParent(Building.transform, worldPositionStays: false);
+        audioClientObject.SetActive(false);
     }
 }
--- RealRadio/Components/Radio/BuildingRadioManager.cs
+++ RealRadio/Components/Radio/BuildingRadioManager.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using RealRadio.Components.Buildings;
 using ScheduleOne.DevUtilities;
+using ScheduleOne.Map;
 using UnityEngine;
 
 namespace RealRadio.Components.Radio;
@@ -10,16 +13,52 @@ public class BuildingRadioManager : NetworkSingleton<BuildingRadioManager>
     [field: SerializeField]
     public GameObject RadioProxyPrefab { get; private set; } = null!;
 
+    public Dictionary<int, NPCEnterableBuilding> Buildings { get; private set; } = null!;
+
     public override void Awake()
     {
         base.Awake();
 
         if (RadioProxyPrefab == null)
             throw new InvalidOperationException("RadioProxyPrefab is null");
+
+        var buildings = FindObjectsOfType<NPCEnterableBuilding>();
+        Buildings = new(capacity: buildings.Length);
+
+        foreach (var building in buildings)
+        {
+            // Mick's house seems to have double components added for some reason, so we need to check if the building already exists
+            if (!Buildings.TryAdd(GetBuildingHash(building), building))
+            {
+                Plugin.Logger.LogWarning($"Building already exists: {building.GUID} - {building.BuildingName}");
+            }
+        }
+    }
+
+    public static int GetBuildingHash(NPCEnterableBuilding building)
+    {
+        unchecked
+        {
+            return building.GUID.GetHashCode() * 31 * building.BuildingName.GetHashCode();
+        }
     }
 
     public override void Start()
     {
         base.Start();
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+
+        foreach (var building in Buildings.Values)
+        {
+            var proxy = Instantiate(RadioProxyPrefab, parent: transform);
+            proxy.name = $"{proxy.name} - {building.name}";
+            proxy.GetComponent<BuildingRadioProxy>().Building = building;
+
+            Spawn(proxy);
+        }
     }
 }
