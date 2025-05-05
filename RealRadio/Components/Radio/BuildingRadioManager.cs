975b6350e8c5ab2bfedac0a7d8b6216d1fd5afab
Skipcast <420685+Skippeh@users.noreply.github.com>
2025-05-05 02:00:53 +0200

    Added 'shop' to blacklisted building names

--- RealRadio/Components/Radio/BuildingRadioManager.cs
+++ RealRadio/Components/Radio/BuildingRadioManager.cs
@@ -26,6 +26,7 @@ public class BuildingRadioManager : NetworkSingleton<BuildingRadioManager>
         "office",
         "pharmacy",
         "store",
+        "shop",
         "warehouse",
         "chemical",
         "industrial",
